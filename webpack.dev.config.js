5ef807cded97efb5536d171be786e8b26db029d1
ACPerry <pansj@acloudchina.com>
2018-09-19 20:48:47 +0800

    增加调试源码支持

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -38,5 +38,7 @@ module.exports = {
       actions: path.join(__dirname, 'src/redux/actions'),
       reducers: path.join(__dirname, 'src/redux/reducers'),
     }
-  }
+  },
+
+  devtool: 'inline-source-map'
 };
