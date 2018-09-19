73db992c7b92dddfe747f69eb3f7a9424f26eed4
ACPerry <pansj@acloudchina.com>
2018-09-19 21:03:44 +0800

    增加对图片的支持

--- src/pages/Page1/Page1.js
+++ src/pages/Page1/Page1.js
@@ -2,11 +2,14 @@ import React, {Component} from 'react';
 
 import './Page1.css';
 
+import image from './images/totoro.jpg';
+
 export default class Page1 extends Component {
   render() {
     return (
       <div className="page-box">
         this is Page1~
+        <img src={image}/>
       </div>
     )
   }
